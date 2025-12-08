dea6ddaec64c68d9ce14001bfc32806f921991d1
Creative-Duoc <sam.bueno@duocuc.cl>
2025-12-08 11:44:44 -0300

    Update ApiClient to change BASE_URL for improved backend connectivity

--- app/src/main/java/com/example/geo_moba/data/network/ApiClient.kt
+++ app/src/main/java/com/example/geo_moba/data/network/ApiClient.kt
@@ -5,7 +5,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 object ApiClient {
 
-    private const val BASE_URL = "http://10.0.2.2:8080/"
+    private const val BASE_URL = "ms-db-client-mobile.railway.internal/"
 
     val instance: ApiService by lazy {
         val retrofit = Retrofit.Builder()
